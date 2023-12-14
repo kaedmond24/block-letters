b7e10ef3c0b8248c9e6af3079dd971294a6b8ef2
Kevin Edmond <135303747+kaedmond24@users.noreply.github.com>
2023-12-14 16:23:37 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,8 @@
+# Your first name and last name initials
+print("K   K EEEEE")
+print("K  K  E    ")
+print("K K   E    ")
+print("KK    EEE  ")
+print("K K   E    ")
+print("K  K  E    ")
+print("K   K EEEEEE")
